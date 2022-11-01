aec16ebdf5d98e4a4f4f63776352c5f5ac32d7d1
kiki-kanri <a470666@gmail.com>
2022-11-01 18:54:14 +0800

    修改gunicorn檔案

--- flask/gunicorn.sh
+++ flask/gunicorn.sh
@@ -2,7 +2,7 @@
 . ./env.sh
 
 if [ ! -z $1 ] && [ $1 = "--dev" ]; then
-	gunicorn -b $host:$port --access-logfile '-' --error-logfile '-' --reload -w 1 main.wsgi
+	gunicorn -b $host:$port --reload -t 3000 -w 1 main.wsgi #--access-logfile '-' --error-logfile '-'
 else
-	gunicorn -b $host:$port -k "egg:meinheld#gunicorn_worker" -w $workers main.wsgi
+	gunicorn -b $host:$port -k "egg:meinheld#gunicorn_worker" -t 3000 -w $workers main.wsgi
 fi
